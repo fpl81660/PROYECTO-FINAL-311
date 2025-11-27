26738c39a56fafa5c383023b19ab45f56298d9b3
Fernando Pérez <fpl81660@lasallebajio.edu.mx>
2025-11-27 14:44:43 -0600

    errorHandler

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const setupSwagger = require('./swagger');
+const {logErrors, errorHandler} = require('./middleware/errorHandler');
 
 app.use(express.json());
+app.use(errorHandler); 
+app.use(logErrors); 
 
 
 setupSwagger(app);
@@ -16,10 +19,13 @@ app.get("/nuevaruta", (req, res) => {
   res.send("Hola este es el segundo endpoint");
 });
 
-app.use(express.json());
+app.get('/error', (req,res,next)  => {
+  next(new Error('Algo salio mal'))
+})
 
 app.listen(port, () => {
   console.log("My port is working on: "+ port);
 });
+ 
 
 //El enrutamiento es separar todas las rutas que estoy trabajando.

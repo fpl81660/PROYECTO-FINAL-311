1356ee4e5f64fa4186c1496fe317d35e626a841a
Fernando Pérez <fpl81660@lasallebajio.edu.mx>
2025-11-27 16:56:13 -0600

    porfavor funciona2

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const app = express();
 const port = 3000;
 const setupSwagger = require('./swagger');
 const { logErrors, errorHandler } = require('./middleware/errorHandler');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const Zone = require('./routes/zoneRouter');
 const mongoose = require('mongoose');
+
 app.use(express.json());
+app.use(cors());
 
 setupSwagger(app);
 
@@ -23,21 +24,19 @@ app.get('/error', (req, res, next) => {
   next(new Error('Algo salio mal'))
 });
 
-app.use(errorHandler);
+app.use('/api/Zone', Zone);
+
 app.use(logErrors);
+app.use(errorHandler);
+
+mongoose.connect(
+    'mongodb+srv://ferchoperezlopez10_db_user:REMOVED_SECRET@proyecto-final.jtiaido.mongodb.net/?retryWrites=true&w=majority&appName=proyecto-final')
+    .then(()=>console.log('Connected to DB'))
+    .catch((err)=>console.log(err))
 
 app.listen(port, () => {
   console.log("My port is working on: " + port);
 });
-
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use('/api/Zone', Zone);
-mongoose.connect(
-    'mongodb+srv://ferchoperezlopez10_db_user:fer123@clusterservice311.vogy8ny.mongodb.net/?retryWrites=true&w=majority&appName=clusterservice311')
-    .then(()=>console.log('Connected to DB'))
-    .catch((err)=>console.log(err))
 
 
 //El enrutamiento es separar todas las rutas que estoy trabajando.

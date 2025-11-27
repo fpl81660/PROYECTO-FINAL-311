b7612845f03cbb8ac05995a7c6b54d5a27778693
Fernando Pérez <fpl81660@lasallebajio.edu.mx>
2025-11-27 12:29:19 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hola desde server express");
+});
+
+app.get("/nuevaruta", (req, res) => {
+  res.send("Hola este es el segundo endpoint");
+});
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log("My port is working on: "+ port);
+});
+
+//El enrutamiento es separar todas las rutas que estoy trabajando.

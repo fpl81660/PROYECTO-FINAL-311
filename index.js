7b9219894f79d9972ac7d66eff1c3b25889cf47a
Fernando Pérez <fpl81660@lasallebajio.edu.mx>
2025-11-27 13:10:45 -0600

    swagger2

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const setupSwagger = require('./swagger');
+
+app.use(express.json());
 
 
-
+setupSwagger(app);
 
 app.get("/", (req, res) => {
   res.send("Hola como estas");
